b750e1696ef1f84aace035b05af3da399b62ac56
secondl1ght <secondl1ght@protonmail.com>
2023-07-16 17:23:08 -0600

    sveltekit error demo app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { error } from '@sveltejs/kit';
+
+	throw error(404, { message: 'test' });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
